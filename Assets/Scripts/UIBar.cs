f87bb8e470b1799e7103ed5b8baefb0013c26974
skiddmarc <84139731+skiddmarc@users.noreply.github.com>
2022-05-08 19:40:53 -0400

    fix generation

--- Assets/Scripts/UIBar.cs
+++ Assets/Scripts/UIBar.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class UIBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider slider;
+    public void SetValue(float val)
     {
-        
+        slider.value = val;
+    }
+    public void SetMaxValue(float val)
+    {
+        slider.maxValue = val;
+        slider.value = val;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
