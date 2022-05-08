ef9c5975ddc9d386259303575c2d4b92a13779b5
skiddmarc <84139731+skiddmarc@users.noreply.github.com>
2022-05-07 23:02:23 -0400

    Revamped Asteroid Generation

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -4,33 +4,49 @@ using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using System.Collections;
 
 public class AsteroidSpawner : MonoBehaviour
 {
-
-    public float spawnRadius, minSpacing;
+    
+    public Transform player;
+    public float spawnRadius, minSpacing, secondsBetweenSpawning;
     public List<Sprite> sprites;
     private List<Asteroid> asteroids;
-    private Vector2 currSpawnLocation;
-
+    private float currSpawnAngle;
+    private Vector2 currSpawnPos;
+    
     void Start()
     {
         asteroids = new List<Asteroid>();
+        CreateAsteroid();
+        StartCoroutine("TryToSpawnAsteroid");
 
     }
-    private void Update()
+    IEnumerator TryToSpawnAsteroid()
     {
-
-        asteroids.Select(a => a.Position());
+        for (;;)
+        {
+            currSpawnAngle = Random.Range(0, 2 * Mathf.PI);
+            currSpawnPos.x = spawnRadius * Mathf.Cos(currSpawnAngle) + player.position.x;
+            currSpawnPos.y = spawnRadius * Mathf.Sin(currSpawnAngle) + player.position.y;
+            Debug.Log((asteroids.Select(a => (a.Position() - currSpawnPos).magnitude).All(m => m > minSpacing)));
+            if (asteroids.Select(a => (a.Position() - currSpawnPos).magnitude).All(m => m > minSpacing))
+            { // checks if the asteroid is far enough away from other asteroids
+                CreateAsteroid();
+            }
+            
+            yield return new WaitForSeconds(secondsBetweenSpawning);
+        }
     }
 
-    private void SpawnAsteroid()
+    
+    private void CreateAsteroid()
     {
+        Debug.Log("Created an asteroid");
         float scaleFactor = Random.Range(0.7f, 1.7f);
-        float xCoord = Random.Range(-10, 10);
-        float yCoord = Random.Range(-10, 10);
         float theta = Random.Range(0f, 4f);
-        Asteroid newAsteroid = new Asteroid(new Vector2(xCoord, yCoord), sprites[Random.Range(0, sprites.Count)], theta * 90f, scaleFactor);
+        Asteroid newAsteroid = new Asteroid(currSpawnPos, sprites[Random.Range(0, sprites.Count)], theta * 90f, scaleFactor);
         asteroids.Add(newAsteroid);
     }
 
