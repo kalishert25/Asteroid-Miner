593f8bb66818c9ca40a912c80a8b16857d08a24a
kalishert25 <95220401+kalishert25@users.noreply.github.com>
2022-05-10 13:38:15 -0400

    Added first enemy scripts

--- Assets/Scripts/Enemies/BaseEnemy.cs
+++ Assets/Scripts/Enemies/BaseEnemy.cs
@@ -1,1 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+
+public abstract class BaseEnemy : MonoBehaviour 
+{
+    public Transform player;
+    [SerializeField]
+    private int maxHealth;
+    private int currentHealth = maxHealth;
+    public void TakeDamage(int value)
+    {
+        currentHealth -= value;
+        if(currentHealth <= 0)
+        {
+            HandleDeath();
+        }
+    }
+    private void HandleDeath()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Enemies/SwarmingEnemy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class SwarmingEnemy : BaseEnemy 
+{
+    [SerializeField]
+    private float swarmSpeed;
+    private void Swarm() 
+    {
+        if(player.position.x > gameObject.position.x)
+        {
+            player.position.x -= swarmSpeed
+        }
+        else
+        {
+            player.position.x += swarmSpeed
+        }
+        if(player.position.y > gameObject.position.y)
+        {
+            player.position.y -= swarmSpeed
+        }
+        else
+        {
+            player.position.y += swarmSpeed
+        }
+    }
+
+
+}
