9878577b1e142467a3f984f16641cf7fc1cb7285
crustyDonut <94998524+crustyDonut@users.noreply.github.com>
2022-05-11 19:03:36 -0400

    animated turret (finished)

--- Assets/Scripts/ShootBullet.cs
+++ Assets/Scripts/ShootBullet.cs
@@ -6,8 +6,8 @@ public class ShootBullet : MonoBehaviour
 {
 
 	public Transform firePoint;
-	public GameObject bulletPrefab;
-	public GameObject muzzlePrefab;
+	public GameObject bulletPrefab, muzzlePrefab;
+	public Animator animator;
 
 	public float bulletForce, bulletDespawnTime;
 	public float flashDespawnTime, cooldown;
@@ -17,9 +17,12 @@ public class ShootBullet : MonoBehaviour
 	void Update()
 	{
 
+		animator.SetBool("IsFiring", false);
+
 		if (Input.GetKey(KeyCode.Mouse0) && Time.time > nextFireTime)
 		{
 			nextFireTime = Time.time + cooldown;
+			
 			Shoot();
 			muzzleFlash();
 
@@ -30,6 +33,7 @@ public class ShootBullet : MonoBehaviour
 	void Shoot()
 	{
 
+		animator.SetBool("IsFiring", true);
 		GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 		Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
 		rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
