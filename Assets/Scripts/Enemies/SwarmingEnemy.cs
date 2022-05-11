1f6bc78b86ebfdd6c12d01034b1d4081f06f8578
skiddmarc <84139731+skiddmarc@users.noreply.github.com>
2022-05-11 15:51:44 -0400

    Update SwarmingEnemy.cs

--- Assets/Scripts/Enemies/SwarmingEnemy.cs
+++ Assets/Scripts/Enemies/SwarmingEnemy.cs
@@ -11,23 +11,23 @@ public class SwarmingEnemy : BaseEnemy
     {
         base.Start();
     }
-    private void FixedUpdate()
+    private void Update()
     {
         if (player.position.x > transform.position.x)
         {
-            transform.position += Vector3.right * swarmSpeed * Time.fixedDeltaTime;
+            transform.position += Vector3.right * swarmSpeed * Time.deltaTime;
         }
         else
         {
-            transform.position += Vector3.left * swarmSpeed * Time.fixedDeltaTime;
+            transform.position += Vector3.left * swarmSpeed * Time.deltaTime;
         }
         if (player.position.y > transform.position.y)
         {
-            transform.position += Vector3.up * swarmSpeed * Time.fixedDeltaTime;
+            transform.position += Vector3.up * swarmSpeed * Time.deltaTime;
         }
         else
         {
-            transform.position += Vector3.down * swarmSpeed * Time.fixedDeltaTime;
+            transform.position += Vector3.down * swarmSpeed * Time.deltaTime;
         }
     }
 
